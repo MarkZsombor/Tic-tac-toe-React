abbced7af4d0f157f8c895de915bce2041999aa8
Mark Zsombor <markszsombor@gmail.com>
2018-02-25 14:47:27 -0800

    testing visual studio git features

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
+// Need to break components into separate files
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
